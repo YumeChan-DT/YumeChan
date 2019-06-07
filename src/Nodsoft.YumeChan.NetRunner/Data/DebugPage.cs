9a52a94b9ecb806021826d839a7f9532bc0e498e
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-06-07 02:52:08 +0200

    Add Debug Page + Project Logging Extensions
    Was used to test Environment Variables for #3 resolution.

---
+++ src/Nodsoft.YumeChan.NetRunner/Data/DebugPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nodsoft.YumeChan.NetRunner.Data
+{
+	public static class DebugPage
+	{
+		public static string DebugEnvVar { get; } = "WhereIsThatCake";
+		public static string DebugEnvValue { get => ReadDebug(); }
+
+		public static string ReadDebug()
+		{
+			return Environment.GetEnvironmentVariable(DebugEnvVar);
+		}
+	}
+}
