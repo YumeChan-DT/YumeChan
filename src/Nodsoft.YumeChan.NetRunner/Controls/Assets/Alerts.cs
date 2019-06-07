f545aa3736671e5b1c2293f704ce9450b05bea75
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-06-07 15:42:45 +0200

    Add Bootstrap Assets consts
    Easier access to Button and Alerts values

---
+++ src/Nodsoft.YumeChan.NetRunner/Controls/Assets/Alerts.cs
@@ -0,0 +1,18 @@
+﻿// https://getbootstrap.com/docs/4.3/components/alerts/
+
+namespace Nodsoft.YumeChan.NetRunner.Controls.Assets
+{
+	public static class Alerts
+	{
+		public const string
+			primary = "alert alert-primary",
+			secondary = "alert alert-secondary",
+			success = "alert alert-success",
+			danger = "alert alert-danger",
+			warning = "alert alert-warning",
+			info = "alert alert-info",
+			light = "alert alert-light",
+			dark = "alert alert-dark";
+	}
+}
+
