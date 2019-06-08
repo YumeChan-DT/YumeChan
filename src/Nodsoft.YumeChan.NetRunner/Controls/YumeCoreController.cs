b3d0c057e5390577e2dbb659c31670295e9820f4
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-06-08 12:59:57 +0200

    Add Status Alert in BotControls page

--- src/Nodsoft.YumeChan.NetRunner/Controls/YumeCoreController.cs
+++ src/Nodsoft.YumeChan.NetRunner/Controls/YumeCoreController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 
 using Nodsoft.YumeChan.Core;
+using Nodsoft.YumeChan.NetRunner.Controls.Assets;
 using static Nodsoft.YumeChan.NetRunner.Services;
 
 
@@ -8,6 +9,25 @@ namespace Nodsoft.YumeChan.NetRunner.Controls
 {
     public static class YumeCoreController
     {
+		public static async Task<string[]> DisplayStatusAlert()
+		{
+			switch (BotService.CoreState)
+			{
+				case YumeCoreState.Offline:
+					return new string[] { Alerts.danger , "Bot is offline." };
+				case YumeCoreState.Online:
+					return new string[] { Alerts.success, "Bot is online." };
+				case YumeCoreState.Starting:
+					return new string[] { Alerts.info, "Bot is starting..." };
+				case YumeCoreState.Stopping:
+					return new string[] { Alerts.warning, "Bot is Stopping..." };
+				case YumeCoreState.Reloading:
+					return new string[] { Alerts.warning, "Bot is Reloading..." };
+				default:
+					return new string[] { Alerts.danger, "Bot Status is Unknown." };
+			}
+		}
+
 		public static async Task StartBotButton()
 		{
 			if (BotService.CoreState == YumeCoreState.Offline)
