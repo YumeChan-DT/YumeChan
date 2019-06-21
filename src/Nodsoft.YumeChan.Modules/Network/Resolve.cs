c5fe6d0e25cdeb0cc7f6f55e35b5884c8acab51f
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-06-21 02:27:43 +0200

    Implemented ==resolve Command
    This Command allows for a hostname / domain name to be resolved into an IP address, or spout out an error should the input be invalid, or already an IP address.

---
+++ src/Nodsoft.YumeChan.Modules/Network/Resolve.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Nodsoft.YumeChan.Modules.Network
+{
+	[Group("resolve")]
+	public class Resolve : ModuleBase<SocketCommandContext>
+	{
+		[Command]
+		public async Task ResolveCommand(string host)
+		{
+			string hostResolved;
+			string contextUser = Context.User.Mention;
+
+			if (IsIPAddress(host))
+			{
+				await ReplyAsync($"{contextUser}, Isn't ``{host}`` already an IP address ?");
+			}
+			else
+			{
+				try
+				{
+					IPAddress[] a = await Dns.GetHostAddressesAsync(host).ConfigureAwait(false);
+					hostResolved = a.FirstOrDefault().ToString();
+
+					await ReplyAsync($"{contextUser}, Hostname ``{host}`` resolves to IP Address ``{hostResolved}``.");
+				}
+				catch (Exception e)
+				{
+					await ReplyAsync($"{contextUser}, Hostname ``{host}`` could not be resolved.\nException Thrown : {e.Message}");
+				}
+			}
+		}
+
+		internal static bool IsIPAddress(string address)
+		{
+			try
+			{
+				IPAddress.Parse(address);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
