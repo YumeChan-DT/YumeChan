4506126826854b8a5257d18306e9609a096b1961
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-10-09 10:09:34 +0200

    Implemented Authentication
    Authentication is done through Azure AD B2C. One step of the way towards solving issue #6. Authorisation system is still To-Do.

--- src/Nodsoft.YumeChan.NetRunner/Startup.cs
+++ src/Nodsoft.YumeChan.NetRunner/Startup.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,10 +16,20 @@ namespace Nodsoft.YumeChan.NetRunner
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public static void ConfigureServices(IServiceCollection services)
+		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+					.AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
 
@@ -38,6 +51,7 @@ namespace Nodsoft.YumeChan.NetRunner
 			}
 			else
 			{
+				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
@@ -46,8 +60,12 @@ namespace Nodsoft.YumeChan.NetRunner
 			app.UseStaticFiles();
 			app.UseRouting();
 
+			app.UseAuthentication();
+			app.UseAuthorization();
+
 			app.UseEndpoints(endpoints =>
 			{
+				endpoints.MapControllers();
 				endpoints.MapBlazorHub();
 				endpoints.MapFallbackToPage("/_Host");
 			});
