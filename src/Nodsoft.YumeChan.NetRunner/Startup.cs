1596c0c154869cb14026d5e34f4775ce545b3f62
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-12-25 01:16:31 +0100

    Upgrade Framework Version
    Upgrade from .NET Core 3.0 to 3.1 LTS

--- src/Nodsoft.YumeChan.NetRunner/Startup.cs
+++ src/Nodsoft.YumeChan.NetRunner/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +35,11 @@ namespace Nodsoft.YumeChan.NetRunner
 
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
+			services.AddHttpsRedirection(options =>
+			{
+				options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+				options.HttpsPort = 5001;
+			});
 
 			services.AddLogging();
 			services.AddSingleton(LoggerFactory.Create(builder => 
