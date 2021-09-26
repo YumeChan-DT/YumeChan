f326009108fe61d3826e97e3dee2f51403da3595
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-09-26 13:55:59 +0200

    Add Discord Authentication + Roles/Claims

---
+++ src/YumeChan.NetRunner/Services/Authentication/WebAppClaims.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+
+namespace YumeChan.NetRunner.Services.Authentication
+{
+	public class WebAppClaims : IClaimsTransformation
+	{
+		private readonly DiscordClient _client;
+
+		public WebAppClaims(DiscordClient client, IConfiguration config)
+		{
+			_client = client;
+		}
+
+		public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+		{
+			if (principal.Identity.IsAuthenticated)
+			{
+				ClaimsIdentity identity = new();
+				ulong snowflake = Convert.ToUInt64(principal.FindFirstValue(ClaimTypes.NameIdentifier));
+
+				if (_client.CurrentApplication.Owners.Select(u => u.Id).Contains(snowflake))
+				{
+					identity.AddClaim(new(ClaimTypes.Role, UserRoles.Admin));
+				}
+
+				principal.AddIdentity(identity);
+			}
+
+			return Task.FromResult(principal);
+		}
+	}
+}
