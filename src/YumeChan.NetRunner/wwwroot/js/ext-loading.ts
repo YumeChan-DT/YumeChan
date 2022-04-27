9684c7f8c7d8f54736acd86d2255ca779f47ef00
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-04-27 10:02:04 +0200

    Add ext-loader JS/TS methods to NetRunner

---
+++ src/YumeChan.NetRunner/wwwroot/js/ext-loading.ts
@@ -0,0 +1,28 @@
+function loadCss(path: string) {
+    let element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", path);
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
+
+function loadJs(sourceUrl: string) {
+    if (sourceUrl.length === 0) {
+        console.error("Invalid source URL");
+        return;
+    }
+
+    let tag = document.createElement('script');
+    tag.src = sourceUrl;
+    tag.type = "text/javascript";
+
+    tag.onload = function () {
+        console.log("Script loaded successfully");
+    }
+
+    tag.onerror = function () {
+        console.error("Failed to load script");
+    }
+
+    document.body.appendChild(tag);
+}
