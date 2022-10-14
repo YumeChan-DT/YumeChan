f12ebdbfebb1f626d9267fa4454de60ddfa77447
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-10-14 13:10:45 +0200

    Add Dockerfiles for ConsoleRunner & NetRunner

--- src/YumeChan.Core/Services/Config/InterfaceConfigProvider.cs
+++ src/YumeChan.Core/Services/Config/InterfaceConfigProvider.cs
@@ -1,37 +1,56 @@
-﻿using System.IO;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using JetBrains.Annotations;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using YumeChan.Core.Modules;
 using YumeChan.PluginBase.Tools;
 
 namespace YumeChan.Core.Services.Config;
+#nullable enable
 
-public sealed class InterfaceConfigProvider<TConfig> : IInterfaceConfigProvider<TConfig> where TConfig : class
+public abstract class InterfaceConfigProviderBase
 {
-	private readonly ILoggerFactory _loggerFactory;
+	protected static string ConfigPath { get; } = Path.Combine(Directory.GetCurrentDirectory(), "config");
+	protected static PhysicalFileProvider? ConfigFileProvider { get; set; }
+}
+
+public sealed class InterfaceConfigProvider<TConfig> : InterfaceConfigProviderBase, IInterfaceConfigProvider<TConfig> where TConfig : class
+{
 	private const string FileExtension = ".json";
-	private InterfaceWritableConfigWrapper<TConfig> _configWrapper;
-	private static readonly PhysicalFileProvider _configFileProvider = new(Path.Combine(Directory.GetCurrentDirectory(), "config"));
+	
+	private readonly ILoggerFactory _loggerFactory;
+	private InterfaceWritableConfigWrapper<TConfig>? _configWrapper;
+
 
 	public InterfaceConfigProvider(ILoggerFactory loggerFactory)
 	{
 		_loggerFactory = loggerFactory;
+		
+		// Create the config directory if it doesn't exist
+		if (!Directory.Exists(ConfigPath))
+		{
+			Directory.CreateDirectory(ConfigPath);
+		}
+
+		ConfigFileProvider ??= new(ConfigPath);
 	}
 	
-	public TConfig Configuration { get; set; }
+	public TConfig? Configuration { get; set; }
 
-	public FileInfo ConfigFile { get; private set; }
+	public FileInfo? ConfigFile { get; private set; }
 
 	public TConfig InitConfig(string filename) => InitConfig(filename, false);
 	internal TConfig InitConfig(string filename, bool placeFileAtConfigRoot)
 	{
 		filename += filename.EndsWith(FileExtension) ? string.Empty : FileExtension;
 
-		string pluginDirectory = placeFileAtConfigRoot ? string.Empty : typeof(TConfig).Assembly.GetName().Name;
-		IFileInfo file = _configFileProvider.GetFileInfo(Path.Join(pluginDirectory, filename));
+		string pluginDirectory = placeFileAtConfigRoot ? string.Empty : typeof(TConfig).Assembly.GetName().Name!;
+		IFileInfo file = ConfigFileProvider!.GetFileInfo(Path.Join(pluginDirectory, filename));
+		ConfigFile = file.PhysicalPath is { } path ? new(path) : null;
 		
 		// Instantiate the configuration 
-		_configWrapper = new(JsonConfigProvider<InternalPlugin>.GetConfiguration(file, true, true, _loggerFactory));
+		_configWrapper ??= new(JsonConfigProvider<InternalPlugin>.GetConfiguration(file, true, true, _loggerFactory));
 		
 		return Configuration = _configWrapper.CreateInstance();
 	}
