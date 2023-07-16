d68fe274af0e20448446ae38e59bdb0c7a9f99de
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-07-16 16:41:24 +0200

    feat(InternalPlugin): Update internal plugin manifest
    - Updated the `InternalPlugin` class in `YumeChan.Core.Modules.InternalPlugin.cs`
    - Added properties for version, description, author, author contact, project homepage, source code repository, and icon URI
    - Modified the `Loaded` property to check if the core state is online
    - Updated the subproject commit in `YumeChan.PluginBase`
    
    This commit updates the internal plugin manifest by adding new properties and modifying existing ones. It also updates the subproject commit in `YumeChan.PluginBase`.

--- src/YumeChan.Core/Modules/InternalPlugin.cs
+++ src/YumeChan.Core/Modules/InternalPlugin.cs
@@ -10,12 +10,17 @@ public sealed class InternalPlugin : IPlugin
 {
 	public string AssemblyName { get; } = typeof(YumeCore).Assembly.GetName().Name!;
 	public string DisplayName => "YumeCore Internals";
-		
+	public string Version => YumeCore.CoreVersion;
+	public string Description => "Internal Plugin for YumeChan Core";
+	public string Author => "YumeChan DT (Nodsoft Systems)";
+	public string AuthorContact => "admin@nodsoft.net";
+	public Uri? ProjectHomepage { get; } = new("https://yumechan.app");
+	public Uri? SourceCodeRepository { get; } = new("https://github.com/YumeChan-DT/YumeChan");
+	
 	public bool Loaded => YumeCore.Instance.CoreState is YumeCoreState.Online;
+	public Uri? IconUri { get; set; }
 	public bool ShouldUseNetRunner => false;
 	public bool StealthMode => false;
-		
-	public string Version => YumeCore.CoreVersion;
 		
 	public Task LoadAsync() => Task.CompletedTask;
 	public Task UnloadAsync() => Task.CompletedTask;
