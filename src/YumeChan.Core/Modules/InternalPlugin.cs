f0492574254705edec4bede0cb441138bf89a74d
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-09-21 14:24:25 +0200

    feat(InternalPlugin): Add icon URI
    This commit adds the icon URI to the InternalPlugin class in YumeChan.Core.Modules. The icon URI is set to "https://avatars.githubusercontent.com/u/56922006".

--- src/YumeChan.Core/Modules/InternalPlugin.cs
+++ src/YumeChan.Core/Modules/InternalPlugin.cs
@@ -11,13 +11,15 @@ public sealed class InternalPlugin : IPlugin
 	public string DisplayName => "YumeCore Internals";
 	public string Version => YumeCore.CoreVersion;
 	public string Description => "Internal Plugin for YumeChan Core";
+	
 	public string Author => "YumeChan DT (Nodsoft Systems)";
 	public string AuthorContact => "admin@nodsoft.net";
+	
 	public Uri? ProjectHomepage { get; } = new("https://yumechan.app");
 	public Uri? SourceCodeRepository { get; } = new("https://github.com/YumeChan-DT/YumeChan");
 	
 	public bool Loaded => YumeCore.Instance.CoreState is YumeCoreState.Online;
-	public Uri? IconUri { get; set; }
+	public Uri? IconUri { get; set; } = new("https://avatars.githubusercontent.com/u/56922006");
 	public bool ShouldUseNetRunner => false;
 	public bool StealthMode => false;
 		
